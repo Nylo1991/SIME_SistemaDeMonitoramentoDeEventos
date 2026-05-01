bb3e65b66e46413c11e9bd7610101099a715eeb4
Danilo <nylo.dk@gmail.com>
2026-04-30 21:25:02 -0300

    Implementação de DTOs

---
+++ ApiEventos/DTOs/Request/EventoRequestDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiEventos.DTOs.Request
+{
+    /// <summary>
+    /// DTO utilizado para criação e atualização de eventos
+    /// </summary>
+    public class EventoRequestDto
+    {
+        /// <summary>
+        /// Tipo do evento (Alerta, Falha, etc)
+        /// </summary>
+        public string Tipo_Evento { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Local onde ocorreu o evento
+        /// </summary>
+        public string local_Evento { get; set; } = string.Empty;
+    }
+}
---
+++ ApiEventos/DTOs/Response/ApiResponseDto.cs
@@ -0,0 +1,34 @@
+﻿namespace ApiEventos.DTOs.Response
+{
+    /// <summary>
+    /// Padrão de resposta da API
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ApiResponseDto<T>
+    {
+        /// <summary>
+        /// Código HTTP
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// Indica sucesso ou erro
+        /// </summary>
+        public bool Sucesso { get; set; }
+
+        /// <summary>
+        /// Mensagem da operação
+        /// </summary>
+        public string Mensagem { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Dados retornados
+        /// </summary>
+        public T? Dados { get; set; }
+
+        /// <summary>
+        /// Data da resposta
+        /// </summary>
+        public DateTime Data { get; set; } = DateTime.Now;
+    }
+}
