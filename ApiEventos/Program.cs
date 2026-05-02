19c041f6f3987a30ac9593cd615237cdc4f9a325
Danilo <nylo.dk@gmail.com>
2026-05-02 11:31:29 -0300

    atualização de Program.cs de Api

--- ApiEventos/Program.cs
+++ ApiEventos/Program.cs
@@ -1,40 +1,20 @@
 using ApiEventos.Data;
-using ApiEventos.Config;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// CONFIG
-builder.Services.Configure<ApiConfig>(
-    builder.Configuration.GetSection("ApiConfig"));
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlite("Data Source=eventos.db"));
 
-// DB SQLITE
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=eventos.db"));
-
-// AUTOMAPPER
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-// CONTROLLERS
 builder.Services.AddControllers();
-
-// SWAGGER
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-}
-
-// MIDDLEWARE
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
