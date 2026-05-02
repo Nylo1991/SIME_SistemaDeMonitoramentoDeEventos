5782bd6aed11f76c50bfe5a965d13b182eada8c6
Danilo <nylo.dk@gmail.com>
2026-05-02 08:56:21 -0300

    implementação de nova estrutura da classe Eventos.cs

--- Shared/Evento.cs
+++ Shared/Evento.cs
@@ -3,11 +3,8 @@
     public class Eventos
     {
         public int Id { get; set; }
-
         public string Tipo_Evento { get; set; } = string.Empty;
-
         public string local_Evento { get; set; } = string.Empty;
-
         public DateTime DataHora { get; set; } = DateTime.Now;
     }
 }
